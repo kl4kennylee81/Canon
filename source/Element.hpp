3cfcfee475c6c45327b94aa8917982116b971936
Kenneth Lee <kl488@cornell.edu>
2017-02-25 18:51:38 -0500

    sorry jon

---
+++ source/Element.hpp
@@ -0,0 +1,19 @@
+//
+//  Element.hpp
+//  Canon
+//
+//  Created by Kenneth Lee on 2/25/17.
+//  Copyright © 2017 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#ifndef Element_hpp
+#define Element_hpp
+
+#include <stdio.h>
+
+#endif /* Element_hpp */
+
+enum class Element : int {
+    BLUE,
+    GOLD
+};
---
+++ source/SimpleUnitModel.cpp
@@ -0,0 +1,9 @@
+//
+//  SimpleUnitModel.cpp
+//  Canon
+//
+//  Created by Kenneth Lee on 2/25/17.
+//  Copyright © 2017 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "SimpleUnitModel.hpp"
