c61c0ec86ce754aa0adaba9caf6e6d8b6e96ea63
kfs47 <kfs47@cornell.edu>
2017-03-03 00:20:09 -0800

    asdf

--- source/PathParameters.h
+++ source/PathParameters.h
@@ -1,5 +1,5 @@
-#define MIN_DISTANCE 15
+#define MIN_DISTANCE 20
 #define AI_SPEED 1
-#define VELOCITY 15 //Pixels per frame
-#define RADIUS 8 //Invariant: RADIUS * 2 > VELOCITY
+#define VELOCITY 18 //Pixels per frame
+#define RADIUS 11 //Invariant: RADIUS * 2 > VELOCITY
 #define MAX_PATH_DISTANCE 1000
