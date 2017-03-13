14021fa3ddd231703847c63fed96f1106df10b44
kfs47 <kfs47@cornell.edu>
2017-03-13 12:36:26 -0700

    path

--- source/PathParameters.h
+++ source/PathParameters.h
@@ -1,5 +1,6 @@
-#define MIN_DISTANCE 20
-#define AI_SPEED 1
-#define VELOCITY 30 //Pixels per frame
-#define RADIUS 1 //Invariant: RADIUS * 2 > VELOCITY
-#define TOUCH_RADIUS 100
+//All in world coordinates
+#define MIN_DISTANCE 20.0 //per frame, must be greater than velocity ?
+#define AI_SPEED 1.0 //per frame
+#define VELOCITY 14.0 //per frame
+#define RADIUS 9.0 //(radius * 2) > velocity + 3 ? Basically it needs to be slightly more than half the velocity
+#define TOUCH_RADIUS 100.0
