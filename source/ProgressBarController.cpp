bfab0a8addeb67c175981733250989a07889c952
Kenneth Lee <kl488@cornell.edu>
2017-03-17 02:42:38 -0400

    completed progress bar controller

--- source/ProgressBarController.cpp
+++ source/ProgressBarController.cpp
@@ -50,14 +50,25 @@ bool ProgressBarController::init(std::shared_ptr<GameState> state, std::shared_p
 {
     cugl::Size cameraSize = state->getScene()->getCamera()->getViewport().size;
     
-	// some constants
-	float PERCENT_WIDTH = 0.75f;
-	float PERCENT_HEIGHT = 0.95f;
+	// Constants
+	float PERCENT_WIDTH = 0.8f;
+	float PERCENT_HEIGHT = 0.96f;
 	float BAR_WIDTH = cameraSize.getIWidth() *PERCENT_WIDTH;
+    
+    // world coordinates are fixed bar will be same height in all screen aspect ratios
 	float BAR_HEIGHT = 64.f;
-	float INTER_BAR_GAP = 20.f;
+    
+    // x coordinate in world coordinate is fixed to 1024 so discrete values is okay
+    
+    // since kelly's assets have blankspace padding on begin cap and final cap the gap
+    // needs to be negative to close the whitespace.
+    
+    // TODO specify the padding in the UI element data file
+	float INTER_BAR_GAP = -20.f;
 	
 	float barYPos = cameraSize.getIHeight()*PERCENT_HEIGHT;
+    
+    // padding for the left side to center the bars
 	float barXPadding = cameraSize.getIWidth() * (1.f- PERCENT_WIDTH) / 2.f;
 	
 	// parent pbar node, child of scenenode
@@ -67,6 +78,18 @@ bool ProgressBarController::init(std::shared_ptr<GameState> state, std::shared_p
         _pBarSceneNode->setPosition(Vec2::ZERO);
         state->getScene()->addChild(_pBarSceneNode, 3);
     }
+    
+    auto manager = world->getAssetManager();
+    
+    // TODO when UI Element data file encapsulate it in a data file rather than macros
+    std::shared_ptr<Texture> barForeground = manager->get<Texture>(BAR_FOREGROUND);
+    std::shared_ptr<Texture> beginCap_f = manager->get<Texture>(BEGINCAP_FGRD);
+    std::shared_ptr<Texture> finalCap_f = manager->get<Texture>(FINALCAP_FGRD);
+    
+    std::shared_ptr<Texture> barBackground = manager->get<Texture>(BAR_BACKGROUND);
+    std::shared_ptr<Texture> beginCap_b = manager->get<Texture>(BEGINCAP_BGRD);
+    std::shared_ptr<Texture> finalCap_b = manager->get<Texture>(FINALCAP_BGRD);
+
     
     std::shared_ptr<LevelData> level = world->getLevelData();
 
@@ -84,17 +107,9 @@ bool ProgressBarController::init(std::shared_ptr<GameState> state, std::shared_p
 		// make progress bar object
 		Size size = Size(level->getTime(i) * spacePerUnitTime, BAR_HEIGHT);
         
-		auto manager = world->getAssetManager();
-
-		// current problem - cugl not recognizing color of file texture
-		std::shared_ptr<Texture> barForeground = manager->get<Texture>(BAR_FOREGROUND);
-		std::shared_ptr<Texture> beginCap_f = manager->get<Texture>(BEGINCAP_FGRD);
-		std::shared_ptr<Texture> finalCap_f = manager->get<Texture>(FINALCAP_FGRD);
+        // since progress bar is anchored in the middle add half the width size;
+        nextBarXPos += level->getTime(i)*spacePerUnitTime/2.f;
         
-		std::shared_ptr<Texture> barBackground = manager->get<Texture>(BAR_BACKGROUND);
-		std::shared_ptr<Texture> beginCap_b = manager->get<Texture>(BEGINCAP_BGRD);
-		std::shared_ptr<Texture> finalCap_b = manager->get<Texture>(FINALCAP_BGRD);
-
         std::shared_ptr<ProgressBarModel> progressBar = ProgressBarModel::allocWithCaps(barBackground,beginCap_b,finalCap_b,
                                                                                    barForeground,beginCap_f,finalCap_f,size);
 
@@ -105,8 +120,8 @@ bool ProgressBarController::init(std::shared_ptr<GameState> state, std::shared_p
 		// add to parent node
 		_pBarSceneNode->addChild(progressBar, 3);
 
-		// set previous bar position for cumulative reference
-		nextBarXPos += INTER_BAR_GAP + level->getTime(i)*spacePerUnitTime;
+		// after drawing the bar add the latter half and the gap between bars
+        nextBarXPos += level->getTime(i)*spacePerUnitTime/2.f + INTER_BAR_GAP;
 	}
 	return true;
 }
