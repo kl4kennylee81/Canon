51c763d80fb921779c06c2171cf426e4a6ced6fa
Kenneth Lee <kl488@cornell.edu>
2017-03-23 03:28:10 -0400

    fixed world populate

--- source/World.cpp
+++ source/World.cpp
@@ -34,51 +34,59 @@ std::shared_ptr<GenericAssetManager> World::getAssetManager(){
 
 /** testing function to populate the world without the data files */
 void World::populate() {
-//	_levelData = LevelData::alloc(1);
-//	std::mt19937 rng;
-//	rng.seed(std::random_device()());
-//	// distribution width and height
-//
-//	std::uniform_int_distribution<std::mt19937::result_type> dist9(1, 9);
-//	std::uniform_int_distribution<std::mt19937::result_type> distWidth(0, 31);
-//	std::uniform_int_distribution<std::mt19937::result_type> distHeight(0, 17);
-//
-//	for (int i = 0; i < 10; i++) {
-//		std::shared_ptr<LevelEntry> e = LevelEntry::alloc(dist9(rng), TIME_BETWEEN_SPAWN);
-//		_levelData->addLevelEntry(e);
-//	}
-//
-//	for (int i = 1; i < 10; i++) {
-//		auto wd = WaveData::alloc(1);
-//		for (int j = 0; j<NUMBER_SPAWNS; j++) {
-//			std::uniform_int_distribution<std::mt19937::result_type> dist2(1, 2);
-//			auto ai = _assets->get<AIData>("homing");
-//			std::shared_ptr<WaveEntry> we = WaveEntry::alloc(dist2(rng), distWidth(rng), distHeight(rng), ai);
-//			wd->addWaveEntry(we);
-//		}
-//		std::uniform_int_distribution<std::mt19937::result_type> dist2(1, 2);
-//		auto ai = _assets->get<AIData>("square");
-//		std::shared_ptr<WaveEntry> we = WaveEntry::alloc(dist2(rng), distWidth(rng), distHeight(rng), ai);
-//		wd->addWaveEntry(we);
-//		_waveData.insert(std::make_pair(i, wd));
-//	}
-//
-//	auto od1 = ObjectData::alloc(1, 1, 3, 5, 5, Element::BLUE);
-//	_objectData.insert(std::make_pair(1, od1));
-//
-//	auto od2 = ObjectData::alloc(2, 1, 4, 5, 5, Element::GOLD);
-//	_objectData.insert(std::make_pair(2, od2));
-//
-//	std::shared_ptr<ShapeData> sd = _assets->get<ShapeData>("shape1");
-//	_shapeData.insert({ sd->getUID(),sd });
-//
-//	std::shared_ptr<AnimationData> blueEnemy = _assets->get<AnimationData>("blueEnemyAnimation");
-//	_animationData.insert({ blueEnemy->getUID(),blueEnemy });
-//
-//	std::shared_ptr<AnimationData> yellowEnemy = _assets->get<AnimationData>("redEnemyAnimation");
-//	_animationData.insert({ yellowEnemy->getUID(),yellowEnemy });
-//
-//
+	_levelData = LevelData::alloc();
+	std::mt19937 rng;
+	rng.seed(std::random_device()());
+	// distribution width and height
+
+	std::uniform_int_distribution<std::mt19937::result_type> dist9(1, 9);
+	std::uniform_int_distribution<std::mt19937::result_type> distWidth(0, 31);
+	std::uniform_int_distribution<std::mt19937::result_type> distHeight(0, 17);
+
+	for (int i = 0; i < 10; i++) {
+        std::shared_ptr<LevelEntry> e = LevelEntry::alloc("wave"+std::to_string(dist9(rng)), TIME_BETWEEN_SPAWN);
+		_levelData->addLevelEntry(e);
+	}
+
+    std::shared_ptr<WaveEntry> we;
+	for (int i = 1; i < 10; i++) {
+		auto wd = WaveData::alloc();
+		for (int j = 0; j<NUMBER_SPAWNS; j++) {
+			std::uniform_int_distribution<std::mt19937::result_type> dist2(1, 2);
+            if (dist2(rng) == 1){
+                we = WaveEntry::alloc("object1", distWidth(rng), distHeight(rng));
+            } else {
+                we = WaveEntry::alloc("object2", distWidth(rng), distHeight(rng));
+            }
+			wd->addWaveEntry(we);
+		}
+		std::uniform_int_distribution<std::mt19937::result_type> dist2(1, 2);
+        std::shared_ptr<WaveEntry> we;
+        if (dist2(rng) == 1){
+            we = WaveEntry::alloc("object1",distWidth(rng), distHeight(rng));
+        } else {
+            we = WaveEntry::alloc("object2", distWidth(rng), distHeight(rng));
+        }
+		wd->addWaveEntry(we);
+		_waveData.insert(std::make_pair("wave"+std::to_string(i), wd));
+	}
+
+	auto od1 = ObjectData::alloc("shape1","blueEnemyAnimation","square", Element::BLUE);
+	_objectData.insert(std::make_pair("object1", od1));
+
+    auto od2 = ObjectData::alloc("shape1", "redEnemyAnimation", "homing", Element::GOLD);
+	_objectData.insert(std::make_pair("object2", od2));
+
+	std::shared_ptr<ShapeData> sd = _assets->get<ShapeData>("shape1");
+	_shapeData.insert({"shape1",sd });
+
+	std::shared_ptr<AnimationData> blueEnemy = _assets->get<AnimationData>("blueEnemyAnimation");
+	_animationData.insert({ "blueEnemyAnimation",blueEnemy });
+
+	std::shared_ptr<AnimationData> yellowEnemy = _assets->get<AnimationData>("redEnemyAnimation");
+	_animationData.insert({ "redEnemyAnimation",yellowEnemy });
+
+
 }
 
 bool World::init(std::shared_ptr<GenericAssetManager> assets){
