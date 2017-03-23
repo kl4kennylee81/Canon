9870f5b3e86eb8004bf87402f2b2cc1a3ac9a1f2
kfs47 <kfs47@cornell.edu>
2017-03-23 16:02:40 -0700

    asdf

--- source/PathAIData.cpp
+++ source/PathAIData.cpp
@@ -1,5 +1,6 @@
 #include "PathAIData.hpp"
 #include "PathAI.hpp"
+#include "Util.hpp"
 
 using namespace cugl;
 
@@ -27,32 +28,15 @@ PathType getPathTypeFromString(const std::string& str) {
 	return PathType::NONE;
 }
 
-std::vector<std::string> split(const std::string& s, const char& c)
-{
-	std::string buff{ "" };
-	std::vector<std::string> v;
-
-	for (auto n : s)
-	{
-		if (n != c) {
-			buff += n;
-		}
-		else if (n == c && buff != "") {
-			v.push_back(buff); buff = "";
-		}
-	}
-	if (buff != "") v.push_back(buff);
-	return v;
-}
 
 Vec2 getTupleFromString(const std::string& s) {
 	std::string str = s.substr(1, s.size() - 2);
-	auto nums = split(str, ',');
+	auto nums = Util::split(str, ',');
 	return Vec2::Vec2(std::stoi(nums.at(0)), std::stoi(nums.at(1)));
 }
 
 std::vector<Vec2> getPathFromString(const std::string& str) {
-	auto strings = split(str, ' ');
+	auto strings = Util::split(str, ' ');
 	std::vector<Vec2> path;
 	for (auto it : strings) {
 		path.push_back(getTupleFromString(it));
