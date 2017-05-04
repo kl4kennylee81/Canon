e5757f9c8425cf48dcf94c1e2f918ca62b7e1465
Kenneth Lee <kl488@cornell.edu>
2017-05-04 09:39:37 -0400

    had to make it in terms of physicsCoordinates for everything in data

--- assets/fixup.py
+++ assets/fixup.py
@@ -1,40 +1,47 @@
 import json   
-json_txt = """
-{
-"ROLE_NAME": {
-	"FOO": {
-		"download_url": "http: //something.staging/12345/buzz.zip"
-	},
-	"BAR": {
-		"download_url": "http: //something.staging/12345/fizz.zip"
-	},
-	"download_url": "http: //something.staging/12345/fizzbuzz.zip",
-	"db_name": "somedb",
-	"db_server": "dbserver.staging.dmz",
-	"plugin": {
-		"server_url": "http: //lab.staging.corp/server/"
-	}
-}
-}
-"""
-data = json.loads(json_txt)
+import json
+import os
+import os.path
+
+# json_txt = """
+# {
+# "ROLE_NAME": {
+# 	"FOO": {
+# 		"download_url": "http: //something.staging/12345/buzz.zip"
+# 	},
+# 	"BAR": {
+# 		"download_url": "http: //something.staging/12345/fizz.zip"
+# 	},
+# 	"download_url": "http: //something.staging/12345/fizzbuzz.zip",
+# 	"db_name": "somedb",
+# 	"db_server": "dbserver.staging.dmz",
+# 	"plugin": {
+# 		"server_url": "http: //lab.staging.corp/server/"
+# 	}
+# }
+# }
+# """
+
 def fixup(adict, k):
+	if type(adict) is not dict:
+		return;
 	for key in adict.keys():
-		if key == k:
-			adict[key] = float(v)/32.0
-		elif type(adict[key]) is dict:
-			fixup(adict[key], k, v)
+		print(key)
+		if type(adict[key]) is dict:
+			fixup(adict[key], k);
 
 		elif type(adict[key]) is list: 
-			for element in adict[key]:
-				fixup(element,k,v)
-
-import pprint
-pprint.pprint( data )
+				if (key == k):
+					newV = []
+					for v in adict[key]:
+						newV.append(float(v)/32.0)
+					adict[key] = newV;
+		elif key == k:
+			adict[key] = float(adict[key])/32.0
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
-for path, subdirs, files in os.walk(dname + os.sep + 'json' + os.sep + 'templates'):
+for path, subdirs, files in os.walk(dname + os.sep + 'json' + os.sep + 'testLevels'):
 	for filename in files:
 		splits = filename.split('.')
 		name, ext = splits[0], splits[1]
@@ -44,5 +51,6 @@ for path, subdirs, files in os.walk(dname + os.sep + 'json' + os.sep + 'template
 				data = json.load(filepath)
 				fixup(data, 'x');
 				fixup(data, 'y');
-
-pprint.pprint( data )    
+				fixup(data, "vertices")
+			with open(os.path.join(path, filename),"w") as filepath:
+				json.dump(data, filepath, indent=4, sort_keys=True)
