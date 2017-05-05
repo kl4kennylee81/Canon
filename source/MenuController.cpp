27853714b713d00f964d8b2a4940c5eaa11adac8
Kenneth Lee <kl488@cornell.edu>
2017-05-05 09:33:08 -0400

    menu stuff is going wierd

--- source/MenuController.cpp
+++ source/MenuController.cpp
@@ -123,10 +123,12 @@ void MenuController::update(float timestep) {
                     case Mode::MAIN_MENU:
                     {
                         getMenuGraph()->setActiveMenu(action->nextScreen);
+                        break;
                     }
                     case Mode::LEVEL_EDIT:
                     {
                         getMenuGraph()->setActiveMenu(action->nextScreen);
+                        break;
                     }
                     default:
                     {
