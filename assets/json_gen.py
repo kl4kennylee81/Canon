ea9b7f0e66dca3cdfca1dce7d14cf34a212a3cb6
Kelly Yu <euriiko@gmail.com>
2017-05-08 15:20:25 -0400

    added in lock texture for menus

--- assets/json_gen.py
+++ assets/json_gen.py
@@ -60,6 +60,9 @@ def modify_json(src_json, file_path_mapping):
         elif 'uiBackgroundKey' in uidata.keys():
           target = 'uiBackgroundKey' 
 
+        # bonus feature LOL make sure key field is correct
+        src_json['UIData'][menu_name]['key'] = menu_name
+
         # make sure appropriate keys exist
         if target != '':
 
@@ -76,9 +79,6 @@ def modify_json(src_json, file_path_mapping):
 
           # update actual menu json
           src_json['UIData'][menu_name][target] = append_name
-
-          # bonus feature LOL make sure key field is correct
-          src_json['UIData'][menu_name]['key'] = menu_name
 
 
 def json_files():
