74785b7b9dcb4163eaebeb397fde648e0ab62d36
Kenneth Lee <kl488@cornell.edu>
2017-05-13 02:40:48 -0400

    working on data of it

--- source/TutorialLevelData.cpp
+++ source/TutorialLevelData.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "TutorialLevelData.hpp"
+
+using namespace cugl;
+
+std::shared_ptr<JsonValue> TutorialLevelData::toJsonValue()
+{
+    std::shared_ptr<JsonValue> tutorialJson = Data::toJsonValue();
+    std::shared_ptr<JsonValue> stepList = JsonValue::allocObject();
+    for(std::string stepDataKey : this->_stepKeys){
+        stepList->appendChild(JsonValue::alloc(stepDataKey));
+    }
+    tutorialJson->appendChild("stepKeys",stepList);
+    tutorialJson->appendChild("levelKey",JsonValue::alloc(this->_levelKey));
+    return tutorialJson;
+}
+
+
+bool TutorialLevelData::preload(const std::string& file)
+{
+    auto reader = JsonReader::allocWithAsset(file.c_str());
+    auto json = reader->readJson();
+    preload(json);
+    return true;
+}
+
+bool TutorialLevelData::preload(const std::shared_ptr<cugl::JsonValue>& json)
+{
+    Data::preload(json);
+	std::vector<std::string> sKeys = json->get("stepKeys")->asStringArray();
+    std::string lKey = json->getString("levelKey");
+    return init(sKeys,lKey);
+}
--- source/TutorialLevelData.hpp
+++ source/TutorialLevelData.hpp
@@ -10,5 +10,30 @@
 #define TutorialLevelData_hpp
 
 #include <stdio.h>
+#include <cugl/cugl.h>
+#include <vector>
+#include "Data.hpp"
+
+class TutorialLevelData : public Data {
+public:
+    
+    std::vector<std::string> _stepKeys;
+    std::string _levelKey; // the levelKey that this tutorial serves
+    
+    virtual std::shared_ptr<cugl::JsonValue> toJsonValue() override;
+    virtual bool preload(const std::string& file) override;
+    virtual bool preload(const std::shared_ptr<cugl::JsonValue>& json) override;
+    
+    bool init(std::vector<std::string> sKey, std::string lKey) {
+        _stepKeys = sKey;
+        _levelKey = lKey;
+        return true;
+    }
+    
+    static std::shared_ptr<TutorialLevelData> alloc(std::vector<std::string> sKey, std::string lKey) {
+        std::shared_ptr<TutorialLevelData> result = std::make_shared<TutorialLevelData>();
+        return (result->init(sKey,lKey) ? result : nullptr);
+    }
+};
 
 #endif /* TutorialLevelData_hpp */
