75d0d1c9298bd6300fad998ce84f9b6f636192c1
Kenneth Lee <kl488@cornell.edu>
2017-05-17 07:56:51 -0400

    game state enum

---
+++ source/GameStateEnums.cpp
@@ -0,0 +1,9 @@
+//
+//  GameStateEnums.cpp
+//  Canon
+//
+//  Created by Kenneth Lee on 5/17/17.
+//  Copyright © 2017 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "GameStateEnums.hpp"
---
+++ source/GameStateEnums.hpp
@@ -0,0 +1,21 @@
+//
+//  GameStateEnums.hpp
+//  Canon
+//
+//  Created by Kenneth Lee on 5/17/17.
+//  Copyright © 2017 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#ifndef GameStateEnums_hpp
+#define GameStateEnums_hpp
+
+#include <stdio.h>
+#include <cugl/cugl.h>
+
+enum class GameplayState : int {
+    TOTAL_PAUSE, // all controllers are paused
+    RESET, // game will reset
+    TUTORIAL_PAUSE // tutorial is still playing but rest is paused
+};
+
+#endif /* GameStateEnums_hpp */
