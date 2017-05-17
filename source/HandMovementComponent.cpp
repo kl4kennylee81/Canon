3470f90d1b2ebf069c7bcf6b7449939b160e1a93
Kenneth Lee <kl488@cornell.edu>
2017-05-17 04:55:00 -0400

    hand movement works correctly atm

--- source/HandMovementComponent.cpp
+++ source/HandMovementComponent.cpp
@@ -13,7 +13,9 @@ using namespace cugl;
 
 bool HandMovementComponent::init(std::shared_ptr<cugl::JsonValue> json){
     _texture = Texture::allocWithFile(json->getString("texture"));
-    _path = Path::alloc(Path::getPathFromString(json->getString("path")));
+    std::string s = json->getString("path");
+    std::vector<Vec2> temp = Path::getPathFromString(s);
+    _path = Path::alloc(temp);
     _repeat = json->getBool("repeat");
     _speed = json->getFloat("speed");
     return true;
